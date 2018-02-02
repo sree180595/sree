ec477bc856777c514086ae641aca37f05155a451
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-02 13:59:56 +0530

    Create constant.C

---
+++ constant.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the number",a);
+	scanf("%d",&a);
+	if(a<0)
+	printf("the number is vowel");
+	else
+	printf("the number is consonant");
+	return 0;
+}
